824aed14f65b34634200a5706ad741cb3ef815c7
JM Bell <justinmbell1997@gmail.com>
2025-09-06 20:19:47 -0500

    added op Maps to constants file

--- RequestBinConst.java
+++ RequestBinConst.java
@@ -1,3 +1,5 @@
+import java.util.Map;
+
 public interface RequestBinConst {
     public static final String DEFAULT_ENCODING = "ISO-8859-1";
     public static final int SINGLE_FLAG = 1 << 0; // Least significant bit
@@ -5,4 +7,20 @@ public interface RequestBinConst {
     public static final int FEMALE_FLAG = 1 << 2; // weight 2^2
     public static final int MAX_LASTNAME_LEN = 255; // Max length lastname
     public static final int MAX_WIRE_LENGTH  = 1024; // Max length on the" wire"
+    public static final Map<String, Byte> opCodes = Map.of(
+        "-", (byte)0,
+        "+", (byte)1,
+        "&", (byte)2,
+        "|", (byte)3,
+        "*", (byte)4,
+        "/", (byte)5
+    );
+    public static final Map<String, String> opNames = Map.of(
+        "-", "subtraction",
+        "+", "addition",
+        "&", "and",
+        "|", "or",
+        "*", "multiplication",
+        "/", "division"
+    );
 }
