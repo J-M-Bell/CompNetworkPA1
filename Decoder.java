2de1b117d7fc11e715288d5b0e66a0355f13190c
JM Bell <justinmbell1997@gmail.com>
2025-09-09 19:34:38 -0500

    added javadocs to decoder interface

--- Decoder.java
+++ Decoder.java
@@ -1,6 +1,10 @@
 import java.io.*;   // for InputStream and IOException
 import java.net.*;  // for DatagramPacket
 
+/*
+* An interface for decoding packets sent between
+* the client and the server
+*/
 public interface Decoder {
   Request decode(InputStream source) throws IOException;
   Request decode(DatagramPacket packet) throws IOException;
