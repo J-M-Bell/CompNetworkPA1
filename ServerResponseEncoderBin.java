ae60ea578c329c2a422dfb7cc535f7c2978b3fa5
JM Bell <justinmbell1997@gmail.com>
2025-09-09 19:42:38 -0500

    uncommented out server response encoder class

--- ServerResponseEncoderBin.java
+++ ServerResponseEncoderBin.java
@@ -1,33 +1,32 @@
-public class ServerResponseEncoderBin //implements Encoder, RequestBinConst 
-{
+public class ServerResponseEncoderBin implements Encoder, RequestBinConst{
 
-//   private String encoding;  // Character encoding
+  private String encoding;  // Character encoding
 
-//   public ServerResponseEncoderBin() {
-//     encoding = DEFAULT_ENCODING;
-//   }
+  public ServerResponseEncoderBin() {
+    encoding = DEFAULT_ENCODING;
+  }
 
-//   public ServerResponseEncoderBin(String encoding) {
-//     this.encoding = encoding;
-//   }
+  public ServerResponseEncoderBin(String encoding) {
+    this.encoding = encoding;
+  }
 
-//   public byte[] encode(Response response) throws Exception {
+  public byte[] encode(Response response) throws Exception {
 
-//     ByteArrayOutputStream buf = new ByteArrayOutputStream();
-//     DataOutputStream out = new DataOutputStream(buf);
+    ByteArrayOutputStream buf = new ByteArrayOutputStream();
+    DataOutputStream out = new DataOutputStream(buf);
 
     
-//     out.writeByte(0); // Message length placeholder
-//     out.writeShort(response.requestID);
-//     out.writeByte(response.statusCode);
-//     out.writeInt(response.result);
-//     out.flush();
-//     byte[] data = buf.toByteArray();
-//     data[0] = (byte) data.length; // Set the length
-//     for (byte b : data) {
-//       System.out.print(String.format("%02X", b));
-//     }
-//     return data;
-//   }
+    out.writeByte(0); // Message length placeholder
+    out.writeShort(response.requestID);
+    out.writeByte(response.statusCode);
+    out.writeInt(response.result);
+    out.flush();
+    byte[] data = buf.toByteArray();
+    data[0] = (byte) data.length; // Set the length
+    for (byte b : data) {
+      System.out.print(String.format("%02X", b));
+    }
+    return data;
+  }
     
 }
