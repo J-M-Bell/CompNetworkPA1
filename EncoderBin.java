feeb2296598224195e28515df368641212740cb4
JM Bell <justinmbell1997@gmail.com>
2025-09-09 19:32:45 -0500

    implemented encoder name change plus added class file

--- EncoderBin.java
+++ EncoderBin.java
@@ -1,14 +1,14 @@
 import java.io.*;  // for ByteArrayOutputStream and DataOutputStream
 
-public class ClientRequestEncoderBin implements Encoder, RequestBinConst {
+public class EncoderBin implements Encoder, RequestBinConst {
 
   private String encoding;  // Character encoding
 
-  public ClientRequestEncoderBin() {
+  public EncoderBin() {
     encoding = DEFAULT_ENCODING;
   }
 
-  public ClientRequestEncoderBin(String encoding) {
+  public EncoderBin(String encoding) {
     this.encoding = encoding;
   }
 
@@ -32,6 +32,26 @@ public class ClientRequestEncoderBin implements Encoder, RequestBinConst {
     for (byte b : data) {
       System.out.print(String.format("%02X", b));
     }
+    System.out.println("\n");
+    return data;
+  }
+
+  public byte[] encode(Response response) throws Exception {
+    ByteArrayOutputStream buf = new ByteArrayOutputStream();
+    DataOutputStream out = new DataOutputStream(buf);
+
+    
+    out.writeByte(0); // Message length placeholder
+    out.writeInt(response.result);
+    out.writeByte(response.errorCode);
+    out.writeShort(response.requestID);
+    out.flush();
+    byte[] data = buf.toByteArray();
+    data[0] = (byte) data.length; // Set the length
+    for (byte b : data) {
+      System.out.print(String.format("%02X", b));
+    }
+    System.out.println("\n");
     return data;
   }
 }
