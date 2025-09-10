6909375f0526096a8ae2824a37b9a938c5fe5705
JM Bell <justinmbell1997@gmail.com>
2025-09-09 19:40:05 -0500

    added a block of code encoding a response to send to the client

--- RecvUDP.java
+++ RecvUDP.java
@@ -6,8 +6,10 @@ public class RecvUDP {
   public static void main(String[] args) throws Exception {
 
       if (args.length != 1 && args.length != 2)  // Test for correct # of args        
-	  throw new IllegalArgumentException("Parameter(s): <Port> [<encoding>]");
+	  throw new IllegalArgumentException("Parameter(s): <Destination>" +
+					                                " <Port> [<encoding]");
       
+      InetAddress destAddr = InetAddress.getByName(args[0]);
       int port = Integer.parseInt(args[0]);   // Receiving Port
       
       DatagramSocket sock = new DatagramSocket(port);  // UDP socket for receiving      
@@ -15,7 +17,6 @@ public class RecvUDP {
       sock.receive(packet);
       
       // Receive binary-encoded Request                              
-      // RequestDecoder decoder = new ClientRequestDecoderBin();
       Decoder decoder = (args.length == 2 ?   // Which encoding              
 				                new ClientRequestDecoderBin(args[1]) :
 				                new ClientRequestDecoderBin() );
@@ -23,9 +24,23 @@ public class RecvUDP {
 
       Request receivedRequest = decoder.decode(packet);
 
-      System.out.println("Received Binary-Encoded Request");
-      System.out.println(receivedRequest);
+      System.out.println("Received request: \n" + receivedRequest.toString());
       
+      //Make reponse class to calculate result
+      Response response = new Response(receivedRequest.requestID, receivedRequest.operation, receivedRequest.leftOperand, receivedRequest.rightOperand);
+      
+      //Encode response
+
+      Encoder encoder = (args.length == 2 ?   // Which encoding              
+                       new EncoderBin (args[1]) :
+                       new EncoderBin() );
+
+      
+      byte[] codedResponse = encoder.encode(response); // Encode Response
+
+      DatagramPacket message = new DatagramPacket(codedResponse, codedResponse.length, 
+						  destAddr, port);
+      sock.send(message);
       sock.close();
   }
 }
