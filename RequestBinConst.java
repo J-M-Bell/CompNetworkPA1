6abc223d1743045df5cddbbbbb37353843e989b0
JM Bell <justinmbell1997@gmail.com>
2025-09-10 22:59:22 -0500

    added documentation to constants class

--- RequestBinConst.java
+++ RequestBinConst.java
@@ -1,7 +1,13 @@
 import java.util.ArrayList;
 import java.util.List;
 
-
+/**
+ * A class that contains constants
+ * used by other classes.
+ *
+ * @author JM Bell
+ * @version 9/10/25
+ */
 public interface RequestBinConst {
     public static final String DEFAULT_ENCODING = "ISO-8859-1";
     public static final int MAX_WIRE_LENGTH  = 1024; // Max length on the" wire"
