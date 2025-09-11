0fe75ac21ec56b70fdf57b9865c59335a6b0d350
JM Bell <justinmbell1997@gmail.com>
2025-09-10 21:00:42 -0500

    added documentation to encoder class

--- Encoder.java
+++ Encoder.java
@@ -1,3 +1,11 @@
+/**
+ * An interface for creating an encoder
+ * for both a client's request and a 
+ * server's response.
+ *
+ * @author JM Bell
+ * @version 9/10/25
+ */
 public interface Encoder {
   byte[] encode(Request request) throws Exception;
   byte[] encode(Response response) throws Exception;
